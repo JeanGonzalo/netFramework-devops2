08a69471ee67ab436225fbca742b606d1bd43ee9
JeanGonzalo <rjeangonzalo@gmail.com>
2022-09-30 12:03:15 -0500

    feat: zip project

--- zipNet.ps1
+++ zipNet.ps1
@@ -1,11 +1,15 @@
 #Get location project
+param(
+    [Parameter()]
+    [string] $nameProject
+)
+
 $location=Get-Location
-$nameProject="ContosoUniversity"
 $dirToCompress="$location/$nameProject"
 
 #Exclude files or directories
 $dirToExclude= "obj", "Web.config", "$nameProject.csproj", "AzulIS.AzMan.xml", "$nameProject.csproj.user", "Classes", "Controllers", "Helpers", "Models", "Properties"
-$dirDestination="$location/blue.zip"
+$dirDestination="$location/$nameProject.zip"
 
 #Zip binaries
 $file=Get-ChildItem -Path $dirToCompress -Exclude  $dirToExclude
